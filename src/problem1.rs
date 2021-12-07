8012037d5ed175798ea134e69739984b35d4492f
Sky Brewer <jacobrewer@gmail.com>
2021-12-07 16:46:57 +0100

    first problem

---
+++ src/problem1.rs
@@ -0,0 +1,32 @@
+use std::path::Path;
+
+use adventofcode2021::read_lines;
+
+pub fn problem1() -> i32 {
+    let filename = "./src/resources/input01.csv";
+    let path = Path::new(filename);
+    let display = path.display();
+
+    // File hosts must exist in current path before this produces output
+    let lines = match read_lines(filename) {
+        Err(e) => panic!("couldn't open {}: {}", display, e),
+        Ok(file) => file,
+    };
+
+    let mut prev_num = 0;
+    let mut increase_count = 0;
+
+    // Consumes the iterator, returns an (Optional) String
+    for line in lines {
+        if let Ok(article) = line {
+            let new_num = article.parse::<i32>().unwrap();
+            if new_num - prev_num > 0 {
+                increase_count = increase_count + 1;
+            }
+
+            prev_num = new_num;
+        }
+    }
+
+    return increase_count -1 ;
+}
