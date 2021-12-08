7685cd1684592c4ecf54c1d11ec8f07176c64485
Sky Brewer <jacobrewer@gmail.com>
2021-12-08 11:20:16 +0100

    finished both parts of problem 2

--- src/problem1.rs
+++ src/problem1.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 
 use adventofcode2021::read_lines;
 
-pub fn problem1() -> i32 {
+pub fn problem1() -> String {
     let filename = "./src/resources/input01.csv";
     let path = Path::new(filename);
     let display = path.display();
@@ -14,19 +14,29 @@ pub fn problem1() -> i32 {
     };
 
     let mut prev_num = 0;
+    let mut prev_prev_num = 0;
+    let mut prev_prev_prev_num = 0;
     let mut increase_count = 0;
+    let mut three_window_increase = 0;
 
     // Consumes the iterator, returns an (Optional) String
     for line in lines {
         if let Ok(article) = line {
             let new_num = article.parse::<i32>().unwrap();
-            if new_num - prev_num > 0 {
+
+            if new_num - prev_num > 0 && prev_num != 0 {
                 increase_count = increase_count + 1;
             }
 
+            if new_num - prev_prev_prev_num > 0 && prev_prev_prev_num != 0{
+                three_window_increase = three_window_increase + 1;
+            }
+
+            prev_prev_prev_num = prev_prev_num;
+            prev_prev_num = prev_num;
             prev_num = new_num;
         }
     }
 
-    return increase_count -1 ;
+    return format!("a: {}, b: {b} ", increase_count , b = three_window_increase );
 }
