9308b3eedd9a992f312521933fb007f10d7b7e56
Sky Brewer <jacobrewer@gmail.com>
2021-12-08 11:48:48 +0100

    problem 2 done

--- src/problem2.rs
+++ src/problem2.rs
@@ -1,4 +1,4 @@
-use std::{path::Path};
+use std::path::Path;
 
 use adventofcode2021::read_lines;
 
@@ -6,24 +6,24 @@ enum Direction {
     Forward,
     Up,
     Down,
-    Error
+    Error,
 }
 
 struct Instruction {
     direction: Direction,
-    distance: i32
+    distance: i32,
 }
 
 fn parse_instruction(input: String) -> Instruction {
     let v: Vec<&str> = input.split(' ').collect();
     Instruction {
-        direction : match v[0] {
-            "forward" => Direction::Forward, 
-            "up" => Direction::Up, 
+        direction: match v[0] {
+            "forward" => Direction::Forward,
+            "up" => Direction::Up,
             "down" => Direction::Down,
-            &_ => Direction::Error
+            &_ => Direction::Error,
         },
-        distance: v[1].parse::<i32>().unwrap()
+        distance: v[1].parse::<i32>().unwrap(),
     }
 }
 
@@ -41,19 +41,39 @@ pub fn problem2() -> String {
     let mut horizontal_pos = 0;
     let mut depth_pos = 0;
 
+    let mut aim = 0;
+    let mut b_horiz_pos = 0;
+    let mut b_depth_pos = 0;
+
     // Consumes the iterator, returns an (Optional) String
     for line in lines {
         if let Ok(article) = line {
             let instruction = parse_instruction(article);
 
+            // Part A calculation
             match instruction.direction {
                 Direction::Forward => horizontal_pos += instruction.distance,
                 Direction::Down => depth_pos += instruction.distance,
                 Direction::Up => depth_pos -= instruction.distance,
-                Direction::Error => println!("Unable to parse string.")
+                Direction::Error => println!("Unable to parse string."),
+            }
+
+            // Part B calculation
+            match instruction.direction {
+                Direction::Forward => {
+                    b_horiz_pos += instruction.distance;
+                    b_depth_pos += aim * instruction.distance
+                },
+                Direction::Down => aim += instruction.distance,
+                Direction::Up => aim -= instruction.distance,
+                Direction::Error => println!("Unable to parse string."),
             }
         }
     }
 
-    return format!("a: {}", horizontal_pos * depth_pos);
+    return format!(
+        "a: {}, b: {b}",
+        horizontal_pos * depth_pos,
+        b = b_depth_pos * b_horiz_pos
+    );
 }
