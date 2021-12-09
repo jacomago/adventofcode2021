cb0aaa33424ecd06375d994724690b6a250a1597
Sky Brewer <jacobrewer@gmail.com>
2021-12-09 15:21:40 +0100

    setup day 5

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod problem1;
 mod problem2;
 mod problem3;
 mod problem4;
+mod problem5;
 
 fn main() {
     println!("Day 1: {}", problem1::problem1());
     println!("Day 2: {}", problem2::problem2());
     println!("Day 3: {}", problem3::main());
     println!("Day 4: {}", problem4::main());
+    println!("Day 5: {}", problem5::main());
 }
---
+++ src/problem5.rs
@@ -0,0 +1,35 @@
+use std::path::Path;
+
+use adventofcode2021::read_lines;
+
+pub fn main() -> String {
+    file_to_solution("./src/resources/input05.csv")
+}
+
+fn file_to_solution(filename: &str) -> String {
+    let path = Path::new(filename);
+    let display = path.display();
+
+    // File hosts must exist in current path before this produces output
+    let mut lines = match read_lines(filename) {
+        Err(e) => panic!("couldn't open {}: {}", display, e),
+        Ok(file) => file,
+    };
+
+    let mut games: Vec<Vec<Vec<i32>>> = vec![];
+    let mut game: Vec<Vec<i32>> = vec![];
+
+    for line in lines {
+        if let Ok(article) = line {
+        }
+    }
+
+    format!("a: {}, b: {b}", 0, b = 0)
+}
+#[test]
+fn test_example() {
+    assert_eq!(
+        file_to_solution("./src/resources/test/input05.csv"),
+        "a: 5, b: 0"
+    );
+}
