f9b0a180ba7ed4186dae287dbd7d6c5bf6d41330
Sky Brewer <jacobrewer@gmail.com>
2021-12-10 16:33:22 +0100

    day 7 part b

--- src/problem7.rs
+++ src/problem7.rs
@@ -6,19 +6,36 @@ pub fn main() -> String {
     file_to_solution("./src/resources/input07.csv")
 }
 
-fn fuel_cost(crab_positions: &Vec<i32>, pos: i32) -> i32 {
-    crab_positions.into_iter().map(
-        |f| (f - pos).abs()
-    ).sum()
+enum Alg {
+    Basic,
+    Cumulative,
 }
 
-fn calc_cheapest_fuel( crab_positions: &Vec<i32> ) -> (i32, i32) {
+fn fuel_cost(crab_positions: &Vec<i32>, pos: i32, alg: &Alg) -> i128 {
+    crab_positions
+        .into_iter()
+        .map(|f| match alg {
+            Alg::Basic => (f - pos).abs() as i128,
+            Alg::Cumulative => {
+                let n = (f - pos).abs() as i128;
+                n * (n + 1) / 2
+            }
+        })
+        .sum()
+}
+
+fn calc_cheapest_fuel(crab_positions: &Vec<i32>, alg: Alg) -> (i128, i32) {
     let max = crab_positions.into_iter().max().unwrap();
-    let mut min_fuel_cost = max.to_owned() * crab_positions.len() as i32;
+    let mut min_fuel_cost = match alg {
+        Alg::Basic => max.to_owned() as i128 * crab_positions.len() as i128,
+        Alg::Cumulative => {
+            max.to_owned() as i128 * (max.to_owned() as i128 + 1) * (crab_positions.len() as i128) / 2
+        }
+    };
     let mut min_pos = 0;
 
     for pos in 0..max.to_owned() {
-        let fuel_cost = fuel_cost(crab_positions, pos);
+        let fuel_cost = fuel_cost(crab_positions, pos, &alg);
         if fuel_cost < min_fuel_cost {
             min_fuel_cost = fuel_cost;
             min_pos = pos;
@@ -26,7 +43,6 @@ fn calc_cheapest_fuel( crab_positions: &Vec<i32> ) -> (i32, i32) {
     }
 
     (min_fuel_cost, min_pos)
-
 }
 
 fn file_to_solution(filename: &str) -> String {
@@ -51,15 +67,16 @@ fn file_to_solution(filename: &str) -> String {
         }
     }
 
-    let (min_fuel_cost, _) = calc_cheapest_fuel(&crab_positions);
+    let (a, _) = calc_cheapest_fuel(&crab_positions, Alg::Basic);
+    let (b, _) = calc_cheapest_fuel(&crab_positions, Alg::Cumulative);
 
-    format!("a: {}, b: {b}", min_fuel_cost, b = 0)
+    format!("a: {}, b: {b}", a, b = b)
 }
 
 #[test]
 fn test_example() {
     assert_eq!(
         file_to_solution("./src/resources/test/input07.csv"),
-        "a: 37, b: 0"
+        "a: 37, b: 168"
     );
 }
