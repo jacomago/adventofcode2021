7e150b9a5b42006d2da8ea69d164108b4225abb6
Sky Brewer <jacobrewer@gmail.com>
2021-12-14 11:15:33 +0100

    day 9

--- src/problem9.rs
+++ src/problem9.rs
@@ -1,4 +1,8 @@
-use std::{path::Path, vec};
+use std::{
+    collections::{BinaryHeap, HashSet, VecDeque},
+    path::Path,
+    vec,
+};
 
 use adventofcode2021::read_lines;
 
@@ -6,38 +10,55 @@ pub fn main() -> String {
     file_to_solution("./src/resources/input09.csv")
 }
 
-fn find_low_points(heightmap: &Vec<Vec<i32>>) -> Vec<i32> {
+fn find_low_points(heightmap: &Vec<Vec<i32>>) -> Vec<((usize, usize), i32)> {
     let mut low_points = vec![];
     for row in 0..heightmap.len() {
         for col in 0..heightmap[row].len() {
             if is_low_point(row, col, heightmap) {
-                low_points.push(heightmap[row][col]);
+                low_points.push(((row, col), heightmap[row][col]));
             }
         }
     }
     low_points
 }
 
-fn surrounding_points(row: usize, col: usize, heightmap: &Vec<Vec<i32>>) -> Vec<i32> {
+fn point_surrounding_points(
+    point: ((usize, usize), i32),
+    heightmap: &Vec<Vec<i32>>,
+) -> Vec<((usize, usize), i32)> {
+    surrounding_points(point.0 .0, point.0 .1, heightmap)
+}
+
+fn surrounding_points(
+    row: usize,
+    col: usize,
+    heightmap: &Vec<Vec<i32>>,
+) -> Vec<((usize, usize), i32)> {
     let mut output = vec![];
     if row > 0 {
-        output.push(heightmap[row - 1][col]);
+        output.push(((row - 1, col), heightmap[row - 1][col]));
     }
     if col > 0 {
-        output.push(heightmap[row][col - 1]);
+        output.push(((row, col - 1), heightmap[row][col - 1]));
     }
     if col < heightmap[row].len() - 1 {
-        output.push(heightmap[row][col + 1]);
+        output.push(((row, col + 1), heightmap[row][col + 1]));
     }
     if row < heightmap.len() - 1 {
-        output.push(heightmap[row + 1][col]);
+        output.push(((row + 1, col), heightmap[row + 1][col]));
     }
     output
 }
 
 fn is_low_point(row: usize, col: usize, heightmap: &Vec<Vec<i32>>) -> bool {
     let surrounding_points = surrounding_points(row, col, heightmap);
-    if heightmap[row][col] < *surrounding_points.iter().min().unwrap() {
+    if heightmap[row][col]
+        < surrounding_points
+            .iter()
+            .map(|f| f.to_owned().1)
+            .min()
+            .unwrap()
+    {
         return true;
     }
     false
@@ -69,14 +90,52 @@ fn file_to_solution(filename: &str) -> String {
     }
 
     let low_points = find_low_points(&heightmap);
-    let a: i32 = low_points.iter().map(|f| f + 1).sum();
+    let a: i32 = low_points.iter().map(|f| f.to_owned().1 + 1).sum();
 
-    format!("a: {}, b: {b}", a, b = 0)
+    let basins = find_basins(&heightmap, &low_points);
+    let mut basin_sizes: BinaryHeap<usize> = basins.into_iter().map(|f| f.len()).collect();
+    let b = basin_sizes.pop().unwrap() * basin_sizes.pop().unwrap() * basin_sizes.pop().unwrap();
+
+    format!("a: {}, b: {b}", a, b = b)
 }
+
+fn find_basins(
+    heightmap: &Vec<Vec<i32>>,
+    low_points: &Vec<((usize, usize), i32)>,
+) -> Vec<HashSet<((usize, usize), i32)>> {
+    let mut basins = vec![];
+    for low_point in low_points {
+        basins.push(find_basin_points(heightmap, low_point));
+    }
+    basins
+}
+
+fn find_basin_points(
+    heightmap: &Vec<Vec<i32>>,
+    low_point: &((usize, usize), i32),
+) -> HashSet<((usize, usize), i32)> {
+    let mut basin_points = HashSet::new();
+    let mut points_to_visit = VecDeque::from([low_point.to_owned()]);
+
+    while !points_to_visit.is_empty() {
+        let current_point = points_to_visit.pop_back().unwrap();
+        basin_points.insert(current_point);
+
+        let surrounding_points = point_surrounding_points(current_point.to_owned(), heightmap);
+
+        for point in surrounding_points {
+            if !(point.1 == 9 || basin_points.contains(&point)) {
+                points_to_visit.push_back(point);
+            }
+        }
+    }
+    basin_points
+}
+
 #[test]
 fn test_example() {
     assert_eq!(
         file_to_solution("./src/resources/test/input09.csv"),
-        "a: 15, b: 0"
+        "a: 15, b: 1134"
     );
 }
